57722112fea73370f8ef3356e2d10e4a9544e5f7
Naveen Naik <naveennaik075@gmail.com>
2016-08-06 16:28:16 +0530

    First Commit

---
+++ src/qsp/Demo1.java
@@ -0,0 +1,18 @@
+package qsp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+          @Test
+          public void method1(){
+        	  
+        	  WebDriver driver = new FirefoxDriver();
+        	  driver.get("https://www.google.co.in/");
+        	  String str = driver.getTitle();
+        	  Reporter.log(str, true);
+        	  driver.close();
+          }
+}
