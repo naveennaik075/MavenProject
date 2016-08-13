f728b18908d4b61f44a364331c52041533b05286
Naveen Naik <naveennaik075@gmail.com>
2016-08-13 18:35:06 +0530

    Extent Report demo

--- src/qsp/Demo1.java
+++ src/qsp/Demo1.java
@@ -6,9 +6,17 @@ import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class Demo1 {
+	      ExtentReports e = new ExtentReports("./ExtentReport/ExtentReports.html");
           @Test
           public void method1(){
+        	//  ExtentReports e = new ExtentReports("./ExtentReports.html");
+        	  ExtentTest et = e.startTest("method1");
+        	  et.log(LogStatus.PASS,"Hi ExtentTest passed");
         	  
         	  WebDriver driver = new FirefoxDriver();
         	  driver.get("https://www.google.co.in/");
@@ -16,5 +24,22 @@ public class Demo1 {
         	  Reporter.log(str, true);
         	 // Assert.fail();
         	  driver.close();
+        	  e.endTest(et);
+        	 // e.flush();
+          }
+          @Test
+          public void method2(){
+        	  //ExtentReports e = new ExtentReports("./ExtentReports.html");
+        	  ExtentTest et = e.startTest("method2");
+        	  et.log(LogStatus.FAIL,"Hi ExtentTest failed");
+        	  
+        	  WebDriver driver = new FirefoxDriver();
+        	  driver.get("https://www.google.co.in/");
+        	  String str = driver.getTitle();
+        	  Reporter.log(str, true);
+        	 // Assert.fail();
+        	  driver.close();
+        	  e.endTest(et);
+        	  e.flush();
           }
 }
